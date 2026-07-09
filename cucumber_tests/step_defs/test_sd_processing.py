68ee8f9dc007b6e63114196cde4c5f9ebd793d80
Maerodrim <43945193+Maerodrim@users.noreply.github.com>
2026-07-09 18:44:55 +0500

    test(cucumber): implement sd-processing feature tests and golden test data

---
+++ cucumber_tests/step_defs/test_sd_processing.py
@@ -0,0 +1,5 @@
+from pytest_bdd import scenarios
+from cucumber_tests.shared_steps.common_steps import *
+from cucumber_tests.shared_steps.unified_pipeline_steps import *
+
+scenarios('../features/sd-processing.feature')
